636235520379a7ff26847aa3fb6a5f9df0ff04e0
yahya123123123 <loverperd3s@gmail.com>
2026-07-01 15:47:44 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,18 @@
-const CACHE_NAME = "interactivebooks-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./books.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "interactivebooks-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
